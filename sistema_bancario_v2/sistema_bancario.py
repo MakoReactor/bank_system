087d9d8da2b15101369a3e4e3760b885a51f4948
MakoReactor <douglas.makubex@gmail.com>
2025-07-12 23:56:25 -0300

    Funções depositar, sacar completas e extrato funcional

--- sistema_bancario_v2/sistema_bancario.py
+++ sistema_bancario_v2/sistema_bancario.py
@@ -1,21 +1,50 @@
 def menu():
     inicio = f"""
-    {"MENU".center(40, "=")}
-    1 - para depositar
-    2 - para sacar
-    3 - para extrato
-    4 - criar usuário
-    5 - criar conta corrente
-    6 - sair
+{"MENU".center(40, "=")}
+1 - para depositar
+2 - para sacar
+3 - para extrato
+4 - criar usuário
+5 - criar conta corrente
+6 - sair
 
-    {"=".center(40, "=")}
+{"=".center(40, "=")}
     
-    """
+"""
     return inicio
 
 
 def depositar(saldo, valor, extrato):
+    if valor <= 0:
+        return False
+    else:
+        saldo += valor
+        extrato += f"Depósito R${valor:28.2f}+\n"
+        return saldo, extrato
+
+def sacar(*, saldo, valor, extrato, valor_limite_saque, limite_qntde_saque, qntde_saques_efetuada):
+    if valor <=0:
+        print("Valor inválido para o saque.")
+        return False
+    
+    if qntde_saques_efetuada == limite_qntde_saque:
+        print("Qnatidade de Saques diários excedida.")
+        return False
+    
+    if valor > valor_limite_saque:       
+        print("Valor excede o valor permitido por saque!")
+        return False
+    
+    if valor > saldo:
+        print("Saldo insuficiente!")
+        return False
+
+    saldo = saldo - valor    
+    extrato = extrato + f"Saque R${valor:31.2f}-\n"
+
     return saldo, extrato
+    
+
 
 
 def exibir_extrato(extrato):
@@ -23,15 +52,67 @@ def exibir_extrato(extrato):
 
 
 def main():
+    # iniciando variáveis
     saldo = 0.0
+    extrato = ""
+    qntde_saques_efetuada = 0
+
+    # Constantes
+    VALOR_LIMITE_SAQUE = 500.0
+    QNTDE_SAQUE_DIARIA = 3
 
     while True:
-        op = input(f"{menu()} => ")
+    
+        op = input(f"{menu()} opção: ")
 
+        # Depositar
         if int(op) == 1 or op == "1":
             valor = float(input("Informe o valor do Depósito: ").strip())
-            # parei aqui
-            depositar(saldo, valor)
+
+            if depositar(saldo, valor, extrato):
+                saldo, extrato = depositar(saldo, valor, extrato)
+            else:
+                print(f"Valor R${valor:.2f} inválido para depósito!")
+                
+        # Sacar
+        elif op == 2 or op == '2':
+            valor = float(input("Informe o valor do Saque: ").strip())
+            if sacar(
+                saldo=saldo, 
+                valor=valor, 
+                extrato=extrato, 
+                valor_limite_saque=VALOR_LIMITE_SAQUE, 
+                limite_qntde_saque=QNTDE_SAQUE_DIARIA, 
+                qntde_saques_efetuada=qntde_saques_efetuada
+                    ):
+
+                saldo, extrato = sacar(
+                    saldo=saldo, 
+                    valor=valor, 
+                    extrato=extrato, 
+                    valor_limite_saque=VALOR_LIMITE_SAQUE, 
+                    limite_qntde_saque=QNTDE_SAQUE_DIARIA, 
+                    qntde_saques_efetuada=qntde_saques_efetuada)
+                qntde_saques_efetuada += 1
+                
+
+            else:
+                print("Saque 'NÃO' efetuado!")
+
+            
+
+          
+        # Extrato
+        elif op == 3 or op == '3':
+            print(f"{'Extrato'.center(40, '=')}")
+            if extrato:
+                print(extrato)
+            else:
+                print("Sem Trasações financeiras")
+            print(f"\nSaldo R${saldo:31.2f}")
+            print(f"{'='.center(40, '=')}")
+
+        # Sair
         elif int(op) == 6 or op == "6":
             print("Obrigado por usar nosso sistema!")
             break
