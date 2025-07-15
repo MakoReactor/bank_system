df2f11bcd4247177588d5be5f5a606729a6501db
MakoReactor <douglas.makubex@gmail.com>
2025-07-15 00:03:56 -0300

    adicionado as funções de criar conta, listar usuários e listar contas

--- sistema_bancario_v2/sistema_bancario.py
+++ sistema_bancario_v2/sistema_bancario.py
@@ -6,7 +6,9 @@ def menu():
 3 - para extrato
 4 - criar usuário
 5 - criar conta corrente
-6 - sair
+6 - listar contas
+7 - listar usuários
+8 - sair
 
 {"=".center(40, "=")}
     
@@ -94,15 +96,36 @@ def criar_usuario(cpf, usuarios):
 
 def criar_conta(cpf, agencia, numero_conta, contas, usuarios):
     for user in usuarios:
-        if user.get(cpf):
+        if cpf in user:
             usuario = user[cpf]["nome"]
-            conta = numero_conta + 1
+            conta = numero_conta
 
             conta = {conta: {"agencia": agencia, "cpf": cpf, "nome": usuario}}
-
             contas.append(conta)
-            user[cpf]["usuario_conta"].append(conta)
 
+            user[cpf]["usuario_conta"].append(conta)
+            return True
+    
+    # Se chegou até aqui, significa que o CPF não foi encontrado
+    return False
+
+def listar_contas(contas):
+    for i, conta in enumerate(contas, 1):
+        num_conta = list(conta.keys())[0]
+        dados = conta[num_conta]
+        print(f"Nº Conta: {num_conta}")
+        for c, v in dados.items():
+            print(f"    {c}: {v}")
+        print('')
+
+def listar_usuarios(usuarios):
+    for i, usuario in enumerate(usuarios, 1):
+        cpf = list(usuario.keys())[0]
+        dados = usuario[cpf]
+        print(f"CPF: {cpf}")
+        for c, v in dados.items():
+            print(f"    {c}: {v}")
+        print('')
 
 def main():
     # iniciando variáveis
@@ -111,7 +134,7 @@ def main():
     qntde_saques_efetuada = 0
     usuarios = []
     contas = []
-    numero_conta = 0
+    numero_conta = 1
 
     # Constantes
     VALOR_LIMITE_SAQUE = 500.0
@@ -119,8 +142,8 @@ def main():
     AGENCIA = "0001"
 
     while True:
-        print("usuarios", usuarios)
-
+        
+      
         op = input(f"{menu()} opção: ")
 
         # Depositar
@@ -162,16 +185,29 @@ def main():
 
         # Criar usuário
         elif op == 4 or op == "4":
+           
             cpf = input("Digite o seu CPF, apenas números: ")
             criar_usuario(cpf, usuarios)
 
         # Criar conta
-        elif op == 4 or op == "4":
-            # Fazer / finalizar a criação de conta
-            return True
+        elif op == 5 or op == "5":            
+            cpf = input("Digite aqui o CPF para abrir a conta: ")
+            if criar_conta(cpf, AGENCIA, numero_conta, contas, usuarios):
+                numero_conta += 1
+                print("Conta criada com sucesso!")
+            else:
+                print("CPF não encontrado.\nPor favor crie um usuário.")
+        # Listar contas
+        elif op == 6 or op == '6':
+            listar_contas(contas)
 
+        # listar usuários
+        elif op == 7 or op == '7':
+            listar_usuarios(usuarios)
+       
+       
         # Sair
-        elif int(op) == 6 or op == "6":
+        elif int(op) == 8 or op == "8":
             print("Obrigado por usar nosso sistema!")
             break
         else:
