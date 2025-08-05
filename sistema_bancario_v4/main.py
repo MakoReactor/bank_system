c18d9098c288fc1f50e6ae3b9d1f13998b08d053
MakoReactor <douglas.makubex@gmail.com>
2025-08-05 17:25:44 -0300

    melhorias visuais estéticas no extrato

--- sistema_bancario_v4/main.py
+++ sistema_bancario_v4/main.py
@@ -1,10 +1,9 @@
 import functools
 from datetime import datetime
 
-from sistema_bancario import (
-    PessoaFisica, ContaCorrente,
-    Saque, Deposito
-)
+from sistema_bancario import PessoaFisica, ContaCorrente, Saque, Deposito
+
+
 def menu():
     inicio = f"""
 {"MENU".center(40, "=")}
@@ -34,25 +33,26 @@ def log(funcao):
         resultado = funcao(*args, *kwargs)
 
         return resultado
-       
+
     return wrapper
 
 
 # pesquisar pessoa
 
+
 def filtrar_pessoa(cpf, clientes):
-    
     for cliente in clientes:
         if cliente.cpf == cpf:
             return cliente
     return None
+
 
 @log
 def cadastro_cliente(clientes):
     cpf = input("CPF (somente números): ")
 
     cliente = filtrar_pessoa(cpf, clientes)
-            
+
     if cliente:
         print("xxx Cliente já existe! xxx")
         return
@@ -60,7 +60,9 @@ def cadastro_cliente(clientes):
     nome = input("Nome: ")
     data_nascimento = input("Data nascimento (AAAA-M-D)")
     endereco = input("Endereco (Rua, nº - Bairro, Cidade/Estado)")
-    p = PessoaFisica(cpf=cpf, nome=nome, data_nascimento=data_nascimento, endereco=endereco)
+    p = PessoaFisica(
+        cpf=cpf, nome=nome, data_nascimento=data_nascimento, endereco=endereco
+    )
     clientes.append(p)
     print("\n=== Cliente criado com Sucesso! ===")
 
@@ -69,8 +71,8 @@ def cadastro_cliente(clientes):
 def cria_conta(clientes, contas, num_conta):
     cpf = input("Digite o CPF: ")
 
-    cliente = filtrar_pessoa(cpf=cpf, clientes=clientes)    
-    
+    cliente = filtrar_pessoa(cpf=cpf, clientes=clientes)
+
     if cliente:
         conta = ContaCorrente(numero=num_conta, cliente=cliente)
         contas.append(conta)
@@ -78,7 +80,7 @@ def cria_conta(clientes, contas, num_conta):
         print(" === Conta criada com sucesso! ===")
     else:
         print("xxx Cliente não existe, falha ao criar conta. xxx")
-    
+
 
 # Listar Contas
 @log
@@ -88,6 +90,7 @@ def listar_contas(contas):
         print(conta)
     print("=" * 40)
 
+
 @log
 def listar_pessoas(clientes):
     print("=" * 40)
@@ -95,7 +98,7 @@ def listar_pessoas(clientes):
         print(cliente)
         for conta in cliente.contas:
             print(f"{conta}")
-        
+
     print("=" * 40)
 
 
@@ -115,7 +118,8 @@ def depositar(clientes):
                 return
 
     print("xxx Cliente ou Conta não encontrado! xxx")
-            
+
+
 @log
 def sacar(clientes):
     cpf = input("Qual o CPF da conta: ")
@@ -132,32 +136,41 @@ def sacar(clientes):
                 return
 
     print("xxx Cliente ou Conta não encontrado! xxx")
-               
+
+
 @log
 def extrato(clientes):
-    
     cpf = input("Qual o CPF da conta: ")
     num_conta = int(input("Digite o número da Conta: "))
     cliente = filtrar_pessoa(cpf=cpf, clientes=clientes)
 
     if cliente:
-               
         cc = [conta for conta in cliente.contas if conta.numero == num_conta]
         cc = cc[0]
-        
-        saques_realizados = len([transacao for transacao in cc.historico.transacoes if transacao["tipo"] == Saque.__name__])
-        
-        print(f"\n\n{'Extrato'.center(40, "=")}")
-        print(f"Cliente: {cliente.nome}\nSaques Restantes: {cc.limite_saques - saques_realizados}\n\n")
-        
+
+        saques_realizados = len(
+            [
+                transacao
+                for transacao in cc.historico.transacoes
+                if transacao["tipo"] == Saque.__name__
+            ]
+        )
+
+        print(f"\n\n{'Extrato'.center(40, '=')}")
+        print(
+            f"Cliente: {cliente.nome}\nSaques Restantes: {cc.limite_saques - saques_realizados}\n\n"
+        )
+
         for transacao in cc.historico.transacoes:
-            print(f"{transacao['tipo']} R$ {transacao['valor']:15.2f}")
-    print(f"\n\nSaldo: R$ {cc.saldo:15.2f}")
-        
-        
+            if transacao["tipo"] == "Saque":
+                print(f"{transacao['tipo']} R$ {transacao['valor']:18.2f}-")
+            else:
+                print(f"{transacao['tipo']} R$ {transacao['valor']:15.2f}+")
+
+    print(f"\n\nSaldo R$ {cc.saldo:18.2f}")
+
     print("=" * 40)
 
-     
 
 # Início do Programa
 def main():
@@ -165,31 +178,30 @@ def main():
     contas = []
 
     while True:
-
         op = input(f"{menu()} opção: ")
 
         if op == "1":
             print("Deposito")
             depositar(clientes)
 
-        elif op =="2":
+        elif op == "2":
             print("Saque")
             sacar(clientes)
 
         elif op == "3":
             extrato(clientes)
 
-        elif op == "4":        
-            cadastro_cliente(clientes)   
+        elif op == "4":
+            cadastro_cliente(clientes)
 
-        elif op == '5':           
+        elif op == "5":
             num_conta = len(contas) + 1
             cria_conta(clientes, contas, num_conta)
 
-        elif op == "6":            
+        elif op == "6":
             listar_contas(contas)
 
-        elif op == '7':            
+        elif op == "7":
             listar_pessoas(clientes)
 
         elif op == "9":
@@ -197,7 +209,6 @@ def main():
             break
         else:
             print("*** Opção inválida, tente novamente! ***")
-
 
 
 if __name__ == "__main__":
